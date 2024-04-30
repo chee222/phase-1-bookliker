664228bb02e63c895d8ff60786be34f51ec6c69b
Chee <ck_white@outlook.com>
2024-04-30 08:03:40 -0400

    books

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function(list) {
+})
+ function changeImage() {
+  let img = document.getElementById(image);
+  img.src = "";
+ }
